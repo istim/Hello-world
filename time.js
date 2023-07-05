da201be60b87881a48f76138dc6243492aa647e2
Timur Sultanshin <107629609+istim@users.noreply.github.com>
2023-07-05 21:12:19 +0300

    Update magic.js

--- time.js
+++ time.js
@@ -1,11 +1,12 @@
-var txt = "Время идти вперёд!";
+let txt = "Время идти вперёд!";
 
-var el = document.getElementById('myt');
+let el = document.getElementById('myt');
 
 if (typeof el.innerText !== 'undefined') {
-    // IE8-
-    el.innerText = txt;
-} else {
-    // Нормальные браузеры
     el.textContent = txt;
+}
+
+function daiZharu(x){
+  let a = prompt('Изменить текст кнопки:', x);
+  butMakeMagic.innerText = a;
 }
